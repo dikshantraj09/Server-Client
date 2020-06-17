ebac1a4ba7efb863db917ba696fef5bb3d82ea78
WinterMeteor001 <44780302+dikshantraj09@users.noreply.github.com>
2020-06-17 15:40:11 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,8 +5,8 @@ import time
 import sys
 
 s=socket.socket()
-host='192.168.0.107' #Enter the ip address of the machine you are tying to shutdown
-port=96 #Same port as in the server
+host='serveo.net'
+port=9632
 s.connect((host,port))
 print("")
 print("Connected To Server")
